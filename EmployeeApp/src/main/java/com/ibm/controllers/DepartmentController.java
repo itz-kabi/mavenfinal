5831f96ff66183091dec1460998aaf30207bbe68
IBM <IBM@IBM-FULLSTACK7.mshome.net>
2018-07-27 11:09:59 +0530

    last

--- EmployeeApp/src/main/java/com/ibm/controllers/DepartmentController.java
+++ EmployeeApp/src/main/java/com/ibm/controllers/DepartmentController.java
@@ -20,6 +20,11 @@ import com.ibm.model.Department;
 @WebServlet("/Departments")
 public class DepartmentController extends HttpServlet {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		
